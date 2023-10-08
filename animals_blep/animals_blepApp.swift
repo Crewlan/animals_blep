a7fcfed8fae242762d320435c5ea61c95b1665da
Rodrigo Silva <rodrigoa.work@gmail.com>
2023-10-08 18:55:09 -0300

    Initial Commit

---
+++ animals_blep/animals_blepApp.swift
@@ -0,0 +1,17 @@
+//
+//  animals_blepApp.swift
+//  animals_blep
+//
+//  Created by Rodrigo Silva on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct animals_blepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
